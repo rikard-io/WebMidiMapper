65e41171121c402877ec502c4ac27ec12f78d1cb
Rikard Lindström <hi@rikard.io>
2020-03-02 18:05:14 -0500

    cleanup

--- public/background.js
+++ public/background.js
@@ -1,8 +1,7 @@
-
-// listen for our browerAction to be clicked
-chrome.browserAction.onClicked.addListener(function (tab) {
-	// for the current tab, inject the "inject.js" file & execute it
-	chrome.tabs.executeScript(tab.ib, {
-		file: 'main.js'
-	});
+chrome.browserAction.onClicked.addListener(function(tab) {
+	if(tab){
+		chrome.tabs.executeScript(tab.ib, {
+			file: "main.js"
+		});
+	}
 });
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import webMidiMapper from "./webMidiMapper";
-
 import querySelector from "@/utils/querySelector";
 import actions from "./actions";
 import store from "@/store";
-import "./scss/main.scss";
-import gui from './gui';
+import gui from "./gui";
 
 // For chrome extension, if script is already injected (in App.vue),
 // just bring up selection
 if (window.__wmm_enable) {
   window.__wmm_enable();
 } else {
-
   store.autoSave = true;
 
   store.on(
@@ -33,7 +30,11 @@ if (window.__wmm_enable) {
   store.on(
     "update-mapping",
     ({ id, selector, inputId, event, action, value, channel }, old) => {
-      if(actions[old.action.key].clear) actions[old.action.key].clear(querySelector(old.selector), old.action.args)
+      if (actions[old.action.key].clear)
+        actions[old.action.key].clear(
+          querySelector(old.selector),
+          old.action.args
+        );
 
       const targetEls = querySelector(selector);
       webMidiMapper.unmap(id);
